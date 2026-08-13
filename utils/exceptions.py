529d0c05c061558dd8e44802c7ac17628b819b5b
delorachan77-source <delorachan77@gmail.com>
2026-08-14 00:20:38 +0330

    Create exceptions.py

---
+++ utils/exceptions.py
@@ -0,0 +1,11 @@
+class BookNotFoundError(Exception):
+    pass
+
+class ValidationError(Exception):
+    pass
+
+class PermissionDeniedError(Exception):
+    pass
+
+class AIServiceError(Exception):
+    pass
